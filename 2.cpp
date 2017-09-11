0729d7ec9007be2d15aedb229e59bbb2672aeb45
温俊俊 <wenjunjun@meituan.com>
2017-09-11 18:24:23 +0800

    fix

--- 2.cpp
+++ 2.cpp
@@ -12,7 +12,7 @@ int x[MAXSIZE],y[MAXSIZE],z[MAXSIZE];
 int uset[MAXSIZE];  
   
 void makeSet(int size) {  
-    for(int i = 0;i < size;i++)  uset[i] = i;  
+    for(int i = 1;i <= size*2 ;i++)  uset[i] = i;  
 }  
   
 int find(int x) {  
