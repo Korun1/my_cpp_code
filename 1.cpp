c24cb84c0b246bbeaa0e570fb435feabb4b74651
Korun1 <korun.wen@cootek.cn>
2017-09-18 00:08:23 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -10,7 +10,14 @@ class Solution {
         int left = maxPathSumDFS(root->left, res);
         int right = maxPathSumDFS(root->right, res);
         int top = root->val + (left > 0 ? left : 0) + (right > 0 ? right : 0);
-        res = max(res, top);
-        return max(left, right) > 0 ? max(left, right) + root->val : root->val; 
+        res = max(res, top); // 这里是假设以当前节点为根节点时，用其值去更新全局最大值
+        if (max(left, right) > 0){
+            // 这种情况是以左右子树某一条路径和当前路径返回给父节点，进行递归求值
+            return max(left, right) + root->val;
         }
+        else{
+            // 这种情况是左右子树路径和都小于0，则返回此节点的权值给父节点进行递归求值
+            return root->val;
+        }
+    }
 };
