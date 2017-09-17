5384499503fa8b37080ac8d3f299d10de61dee1b
Korun1 <korun.wen@cootek.cn>
2017-09-18 00:27:24 +0800

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -23,12 +23,16 @@ map<int,bool>Map[MAXSIZE<<1];
 
 int main()
 {
-	cin >> n >> m;
+	cin >> n >> m >> q;
 	int Increasing = n;
 	makeSet(n);
-	for (int i=0;i<n;i++){
+	for (int i=0;i<m;i++){
 		cin >> x[i] >> y[i] >> z[i];
-		if (z[i] == 1){
+		if (z[i] == 1){ 
+			// 先把所有的朋友关系划分成不同的圈子，一共有n个点,每出现一对朋友关系，
+			// 只要这两个人之前都没有划分圈子，那么新增一个圈子，代表这两个人的圈子；
+			// 只有这两个人任一已经属于某个圈子，那么只需把另一个人合并入这个圈子;
+			// 若两个人属于不同的圈子，那么把这两个圈子合并。
 			int x_father = find(x[i]);
 			int y_father = find(y[i]);
 			if (x_father == x[i] && y_father == y[i] ){
@@ -54,24 +58,28 @@ int main()
 			}
 		}
 	}
-	for (int i=0;i<n;i++) if(z[i] == 2)  {
+	for (int i=0;i<m;i++) if(z[i] == 2)  {
+		// 敌人关系，把他们的圈子标记为敌人状态
 		int x_father = find(x[i]);
 		int y_father = find(y[i]);
 		Map[ x_father ][ y[i] ] = true;
 		Map[ y_father ][ x[i] ] = true;
 	}
-	for (int i=0;i<m;i++){
+	for (int i=0;i<q;i++){
 		int a,b;
 		cin>>a>>b;
 		int a_father = find(a);
 		int b_father = find(b);
 		if (a_father == b_father){
+			// 首先查询是否同一个圈子，判断是否朋友关系
 			cout<<"朋友关系"<<endl;
 		}
 		else if (Map[a_father][b] == true || Map[b_father][a] == true){
+			// 判断是否敌人关系
 			cout<<"敌人关系"<<endl;
 		}
 		else{
+			// 第三种关系
 			cout<<"不认识"<<endl;
 		}
 	}
